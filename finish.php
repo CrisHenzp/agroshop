e259d8c52c0713c6afa09454dba44dfd54db7b7f
keyncarry1 <ke.lopezm@duocuc.cl>
2023-12-07 00:34:45 -0300

    commit

--- finish.php
+++ finish.php
@@ -1,6 +1,11 @@
-<?php 
+<?php ob_start();
 include('header.php');
-
+// Comprobar si el usuario ha iniciado sesión
+if (!isset($_SESSION['id_usuario'])) {
+    // El usuario no ha iniciado sesión, redirigir a la página de inicio de sesión
+    header('Location: registrar.php');
+    exit;
+}
 ?>
 <br><br><br><br><br><br><br><br><br>
 
@@ -8,9 +13,10 @@ include('header.php');
 <br><br>
 
 <br>
-<a href="historial.compra.php">Ver historial</a>
+
+<a href="historial.compra.php" class="btn btn-primary" ><b>Ver historial</b></a>
+
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
-
-<?php 
+<?php ob_end_flush();
 include('footer.php'); ?>
